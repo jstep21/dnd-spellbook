7b0975b4ab59b3ff883d4ce7e0bbff64610587e5
jstep21 <jstep21@github.com>
2024-03-31 21:51:26 -0400

    added base.html

--- main.py
+++ main.py
@@ -1,38 +1,60 @@
 import requests
 from flask import Flask, render_template, request, redirect, url_for
+from flask_bootstrap import Bootstrap
 
+# init flask and bootstrap
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
+# make api request
 url = "https://dnd5eapi.co/api/spells"
 headers = {'Accept': 'application/json'}
 response = requests.get(url=url, headers=headers)
-
 if response.status_code == 200:
     spells_data = response.json()
     spells = spells_data['results']
 
 
+# home route
 @app.route('/', methods=["GET", "POST"])
 def main():
+    """check if a user makes a search request, if so redirect to search page.
+    if not render home page"""
     if request.method == "GET":
         return render_template(template_name_or_list="index.html",
                                spells_data=spells_data,
                                spells=spells,
-                               request=request
+                               request=request,
+                               bootstrap=bootstrap
                                )
     else:
         spell = request.form['search_spell']
-        return redirect(url_for('search_spells', user_spell=spell))
+        return redirect(url_for('search_spells',
+                                user_spell=spell,
+                                bootstrap=bootstrap))
 
 
+@app.route('/about')
+def about_page():
+    "render about page"
+    return render_template(template_name_or_list='about.html',
+                           )
+
+
+# route to list all spells
 @app.route('/list_spells')
 def list_spells():
+    """render webpage for a list of all spells in spellbook"""
     return render_template(template_name_or_list='spell-list.html',
-                           spells=spells)
+                           spells=spells,
+                           bootstrap=bootstrap)
 
 
+# route for spell search
 @app.route('/spell_search/<user_spell>')
 def search_spells(user_spell):
+    """search for matching spell and make request to the api for spell data.
+    render front-end and provide spell and class data"""
     matching_spell = [spell for spell in spells if spell['name'].lower() == user_spell.lower()]
     spell_url = matching_spell[0]['url']
 
@@ -43,7 +65,8 @@ def search_spells(user_spell):
         classes = [dnd_class['name'] for dnd_class in spell_data['classes']]
         return render_template(template_name_or_list='spell-search.html',
                                spell_data=spell_data,
-                               classes=classes
+                               classes=classes,
+                               bootstrap=bootstrap
                                )
 
 
